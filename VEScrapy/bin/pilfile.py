10f666395b342dd0bd932a57021fc7aafe6204f5
hanwei <hanwei2008123@163.com>
2015-08-21 18:00:25 +0800

    初始化版本库

---
+++ VEScrapy/bin/pilfile.py
@@ -0,0 +1,5 @@
+#!/home/hanwei/SoftWare/Python/ENV/VEScrapy/bin/python
+# EASY-INSTALL-SCRIPT: 'Pillow==2.8.1','pilfile.py'
+__requires__ = 'Pillow==2.8.1'
+import pkg_resources
+pkg_resources.run_script('Pillow==2.8.1', 'pilfile.py')
